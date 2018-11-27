ba165effa2a868a30ab82f8daa04af109dbe8ad7
Rakib <rakibulislam7272@gmail.com>
2018-11-27 12:29:00 +0600

    add title variable

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -27,6 +27,7 @@ class SiteController extends Controller
      */
     public function index()
     {
+        $data['title'] = 'Home';
         $data['sliders'] = Slider::orderBy('slider_id', 'desc')->get();
         $data['product_lists'] = Product::leftJoin('categories', 'categories.cat_id', '=', 'products.cat_id')->get();
         $data['categories'] = Category::where('is_active', 1)->get();
@@ -40,6 +41,7 @@ class SiteController extends Controller
      */
     public function aboutUs()
     {
+        $data['title'] = 'Home';
         $data['about'] = About::where('is_active', 1)->first();
         return view('pages.about-us', $data);
     }
@@ -47,6 +49,7 @@ class SiteController extends Controller
 
     public function productList()
     {
+        $data['title'] = 'Home';
         $data['categories'] = Category::where('is_active', 1)->get();
         $data['product_lists'] = Product::leftJoin('categories', 'categories.cat_id', '=', 'products.cat_id')->get();
         return view('pages.product-list', $data);
@@ -54,6 +57,7 @@ class SiteController extends Controller
 
     public function productListSearch(Request $request)
     {
+        $data['title'] = 'Home';
         $data['categories'] = Category::where('is_active', 1)->get();
         if($request->cat_id == 'all'){
             $data['product_lists'] = Product::leftJoin('categories', 'categories.cat_id', '=', 'products.cat_id')->get();
@@ -65,6 +69,7 @@ class SiteController extends Controller
 
     public function productListById($cat_id)
     {
+        $data['title'] = 'Home';
         $data['categories'] = Category::where('is_active', 1)->get();
         if($cat_id == 'all'){
             $data['product_lists'] = Product::leftJoin('categories', 'categories.cat_id', '=', 'products.cat_id')->get();
@@ -82,6 +87,7 @@ class SiteController extends Controller
      */
     public function show($product_id)
     {
+        $data['title'] = 'Home';
         $data['product_list'] = Product::where('products.product_id', $product_id)->leftJoin('categories', 'categories.cat_id', '=', 'products.cat_id')->first();
         $data['products'] = Product::where('products.product_id', '!=', $product_id)->leftJoin('categories', 'categories.cat_id', '=', 'products.cat_id')->get();
         
@@ -91,6 +97,7 @@ class SiteController extends Controller
     public function contactUs()
     {
         // $data['about'] = About::where('is_active', 1)->first();
+        $data['title'] = 'Home';
         return view('pages.contact-us');
     }
 
